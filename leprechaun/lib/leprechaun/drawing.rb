a03f5377b7dda3ef59b05b47656d90f90b0f1e4c
Joon Ki Choi <chojo@eda.admin.ch>
2015-04-08 10:53:49 +0200

    refactor to use namespace and moved to correct subdirectory lib/leprechaun/

---
+++ leprechaun/lib/leprechaun/drawing.rb
@@ -0,0 +1,17 @@
+module Leprechaun
+  class Drawing
+    NUMBERS = 6
+
+    def draw
+      NUMBERS.times.reduce([]) do |arr, i|
+        arr << single_draw
+      end
+    end
+
+    private
+
+    def single_draw
+      rand(0...49)
+    end
+  end
+end
