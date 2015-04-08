a3bac44bbd11fde2144a34ee0186d240ee35251a
Joon Ki Choi <chojo@eda.admin.ch>
2015-04-08 10:49:43 +0200

    moved class drawing within module leprechaun in a new file

--- leprechaun/lib/leprechaun.rb
+++ leprechaun/lib/leprechaun.rb
@@ -1,17 +1,5 @@
 require "leprechaun/version"
+require "leprechaun/drawing"
 
 module Leprechaun
-  class Drawing
-    NUMBERS = 6
-
-    def draw
-      NUMBERS.times.reduce([]) do |arr, i|
-        arr << single_draw
-      end
-    end
-
-    def single_draw
-      rand(0...49)
-    end
-  end
 end
