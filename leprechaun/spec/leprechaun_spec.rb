5ea3ee44ea796a2dd692fcac5634aa84f4cf4d89
Joon Ki Choi <chojo@eda.admin.ch>
2015-04-08 11:09:04 +0200

    refactor spec

--- leprechaun/spec/leprechaun_spec.rb
+++ leprechaun/spec/leprechaun_spec.rb
@@ -1,23 +1,36 @@
 require 'spec_helper'
 
-describe Leprechaun do
-  it 'has a version number' do
-    expect(Leprechaun::VERSION).not_to be nil
-  end
+module Leprechaun
 
-  it 'generates a lottery drawing with 6 elements' do
-    expect(Leprechaun::Drawing.new.draw.count).to eq(6)
-  end
+  describe Drawing do
+    describe "#draw" do
+      let(:draw) { Leprechaun::Drawing.new.draw }
 
-  it 'contains only numbers' do
-    def is_number?
-      Leprechaun::Drawing.new.draw.all? { |x| x.is_a? Integer  }
+      it 'has a version number' do
+        expect(Leprechaun::VERSION).not_to be nil
+      end
+
+      it "returns an array with 6 elements" do
+        expect(draw).to be_an Array
+        expect(draw.count).to eq(6)
+      end
+
+      it 'contains only numbers' do
+        # def is_number?
+        #   draw.all? { |x| x.is_a? Integer  }
+        # end
+        # expect(is_number?).to eq(true)
+        draw.each do |drawing|
+          expect(drawing).to be_a Integer
+        end
+      end
+
+      it 'each element is less than 50' do
+        draw.each do |drawing|
+          expect(drawing).to be < 50
+        end
+      end
+
     end
-
-    expect(is_number?).to eq(true)
-  end
-
-  xit 'does something useful' do
-    expect(false).to eq(true)
   end
 end
