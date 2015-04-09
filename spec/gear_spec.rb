8df2e21aaa35d0052445ecf3de8899fa1d09da45
Joon Ki Choi <chojo@eda.admin.ch>
2015-04-09 16:25:56 +0200

    after adding 2 additional arguments - rim and tire - to Gear.initialize all existing callers of the method breaks. To efficiently evolve, code must be easy to change.

--- spec/gear_spec.rb
+++ spec/gear_spec.rb
@@ -1,16 +1,20 @@
 require_relative '../lib/gear'
 
 describe Gear do
-  before :each do
-    @small_gear = Gear.new(30, 27)
-    @big_gear = Gear.new(52, 11)
-  end
+  let(:gear)           { Gear.new(52, 11, 29, 2.1) }
+  let(:wheel_diameter) { gear.rim + ( 2 * gear.tire ) }
+  let(:gear_inches)    { wheel_diameter * gear.ratio }
 
   describe "#ratio" do
     it "returns the ratio of chainring and cog" do
-      expect(@small_gear.ratio).to eq(30/27.to_f)
-      expect(@big_gear.ratio).to eq(52/11.to_f)
+      expect(gear.ratio).to eq(52/11.to_f)
     end
   end
   
+  describe "#gear_inches" do
+    it "returns the gear inches" do
+      # expect(gear.gear_inches).to eq(gear.ratio * ((gear.tire * 2) + gear.rim))
+      expect(gear.gear_inches).to eq(156.94545454545457)
+    end
+  end
 end
