ecba61faaf9160786d79e2938f01d86a61c75952
Joon Ki Choi <chojo@eda.admin.ch>
2015-04-20 14:52:18 +0200

    remove obsolete comments

--- lib/gear_better-example.rb
+++ lib/gear_better-example.rb
@@ -12,19 +12,7 @@ class Gear
   #   cog: 11,
   #   wheel: Wheel.new(26, 1.5)).gear_inches
 
-# isolate dependencies
-# if it's not always possible to break all unnecessary dependencies then you
-# should leave the code better than you found it.
-# def initialize(chainring, cog, rim, tire)
-#   @chainring = chainring
-#   @cog = cog
-#   @wheel = Wheel.new(rim, tire)
-# end
-#
-
-
   def gear_inches
-    #ratio * wheel.diameter # this is a simple method and it contains Gear's only reference to wheel.diameter. But in a more complex situation where wheel.diameter is embedded deeply inside a complex method increases its vulnerability. Better:
     # ... a few lines of scary math
     ratio * diameter
     # ... more lines of scary math
